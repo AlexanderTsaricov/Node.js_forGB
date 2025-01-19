3bc51a397e77a79bc34fd696887b9327e9cd837a
Alexandr Vinokurov <salispiligrim@yandex.ru>
2025-01-19 18:05:28 +0500

    add ex whith * in homework_1

--- homework_1/server.js
+++ homework_1/server.js
@@ -1,16 +1,24 @@
+let countHome = 0;
+let countAbout = 0;
 const http = require("http");
 const server = http.createServer((req, res) => {
     console.log("Запрос получен");
     if (req.url === "/") {
+        countHome += 1;
         res.writeHead(200, {
             "Content-Type": "text/html; charset=UTF-8",
         });
-        res.end('<a href="http://localhost:3000/about">About</a>');
+        res.end(
+            `<a href="http://localhost:3000/about">About</a>\n<p>Просмотры: ${countHome}</p>`
+        );
     } else if (req.url === "/about") {
+        countAbout += 1;
         res.writeHead(200, {
             "Content-Type": "text/html; charset=UTF-8",
         });
-        res.end('<a href="http://localhost:3000/">Home</a>');
+        res.end(
+            `<a href="http://localhost:3000/">Home</a>\n<p>Просмотры: ${countAbout}</p>`
+        );
     } else {
         res.writeHead(404, {
             "Content-Type": "text/html; charset=UTF-8",
